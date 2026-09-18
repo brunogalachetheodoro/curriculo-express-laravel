8a3cba4b66d5578d1ed030b270e2832707eaefe5
Bruno Galache Theodoro <bruno.hgt2@gmail.com>
2026-09-18 17:12:34 -0300

    add-transitions

--- resources/js/pages/Auth.vue
+++ resources/js/pages/Auth.vue
@@ -10,7 +10,13 @@
                                     <div class="flex flex-col justify-center items-center">
                                         <input class="bg-secundaria border border-primaria rounded-[8px] my-[5px] placeholder-primaria pl-[10px] w-[90%] p-[5px] lg:p-[8px] lg:w-[75%] placeholder-opacity-60" type="email" :placeholder="t('auth.email')" v-model="login.email">
                                         <input class="bg-secundaria border border-primaria rounded-[8px] my-[5px] placeholder-primaria pl-[10px] w-[90%] p-[5px] lg:p-[8px] lg:w-[75%] placeholder-opacity-60" type="password" :placeholder="t('auth.password')" v-model="login.password">
-                                        <button class="bg-primaria hover:bg-secundaria border-2 border-secundaria rounded-[20px] my-[10px] w-[50%] p-[5px] lg:p-[8px] text-secundaria hover:text-primaria font-semibold transition-colors duration-1000 cursor-pointer" @click="makeLogin">{{ t('auth.loginButton') }}</button>
+                                        <button v-if="!loadingLogin" class="bg-primaria hover:bg-secundaria border-2 border-secundaria rounded-[20px] my-[10px] w-[50%] p-[5px] lg:p-[8px] text-secundaria hover:text-primaria font-semibold transition-colors duration-1000 cursor-pointer" @click="makeLogin">
+                                            {{ t('auth.loginButton') }}
+                                        </button>
+
+                                        <div v-else class="my-[15px] w-[50%] p-[5px] lg:p-[8px] flex justify-center items-center text-secundaria">
+                                            <span class="loader"></span>
+                                        </div>
                                         <p @click="cadastrando = true" class="text-secundaria cursor-pointer">{{ t('auth.noAccount') }}</p>
                                     </div>
                                 </div>
@@ -24,7 +30,13 @@
                                         <input class="bg-secundaria border border-primaria rounded-[8px] my-[5px] placeholder-primaria pl-[10px] w-[90%] p-[5px] lg:p-[8px] lg:w-[75%] placeholder-opacity-60" type="email" :placeholder="t('auth.email')" v-model="register.email">
                                         <input class="bg-secundaria border border-primaria rounded-[8px] my-[5px] placeholder-primaria pl-[10px] w-[90%] p-[5px] lg:p-[8px] lg:w-[75%] placeholder-opacity-60" type="password" :placeholder="t('auth.password')" v-model="register.password">
                                         <input class="bg-secundaria border border-primaria rounded-[8px] my-[5px] placeholder-primaria pl-[10px] w-[90%] p-[5px] lg:p-[8px] lg:w-[75%] placeholder-opacity-60" type="password" :placeholder="t('auth.confirmPassword')" v-model="register.confirm_password">
-                                        <button class="bg-primaria hover:bg-secundaria border-2 border-secundaria rounded-[20px] my-[10px] w-[50%] p-[5px] lg:p-[8px] text-secundaria hover:text-primaria font-semibold transition-colors duration-1000 cursor-pointer" @click="makeRegister">{{ t('auth.registerButton') }}</button>
+                                        <button v-if="!loadingRegister" class="bg-primaria hover:bg-secundaria border-2 border-secundaria rounded-[20px] my-[10px] w-[50%] p-[5px] lg:p-[8px] text-secundaria hover:text-primaria font-semibold transition-colors duration-1000 cursor-pointer" @click="makeRegister">
+                                            {{ t('auth.registerButton') }}
+                                        </button>
+
+                                        <div v-else class="my-[15px] w-[50%] mt- p-[5px] lg:p-[8px] flex justify-center items-center text-secundaria">
+                                            <span class="loader"></span>
+                                        </div>
                                         <p @click="cadastrando = false" class="text-secundaria cursor-pointer">{{ t('auth.alreadyAccount') }}</p>
                                     </div>
                                 </div>
@@ -49,6 +61,7 @@
     const cadastrando = ref(false);
 
     const loadingLogin = ref(false);
+    const loadingRegister = ref(false);
     const leaving = ref(false);
 
     const register = ref({
@@ -92,3 +105,69 @@
 
 
 </script>
+
+<style scoped>
+    .loader {
+        width: 50px;
+        height: 28px;
+        --_g: no-repeat radial-gradient(farthest-side, currentColor 94%, #0000);
+        background:
+            var(--_g) 50% 0,
+            var(--_g) 100% 0;
+        background-size: 12px 12px;
+        position: relative;
+        animation: l23-0 1.5s linear infinite;
+    }
+
+    .loader::before {
+        content: "";
+        position: absolute;
+        height: 12px;
+        aspect-ratio: 1;
+        border-radius: 50%;
+        background: currentColor;
+        left: 0;
+        top: 0;
+        animation:
+            l23-1 1.5s linear infinite,
+            l23-2 0.5s cubic-bezier(0, 200, 0.8, 200) infinite;
+    }
+
+    @keyframes l23-0 {
+        0%, 31% {
+            background-position: 50% 0, 100% 0;
+        }
+
+        33% {
+            background-position: 50% 100%, 100% 0;
+        }
+
+        43%, 64% {
+            background-position: 50% 0, 100% 0;
+        }
+
+        66% {
+            background-position: 50% 0, 100% 100%;
+        }
+
+        79% {
+            background-position: 50% 0, 100% 0;
+        }
+
+        100% {
+            transform: translateX(calc(-100% / 3));
+        }
+    }
+
+    @keyframes l23-1 {
+        100% {
+            left: calc(100% + 7px);
+        }
+    }
+
+    @keyframes l23-2 {
+        100% {
+            top: -0.1px;
+        }
+    }
+</style>
