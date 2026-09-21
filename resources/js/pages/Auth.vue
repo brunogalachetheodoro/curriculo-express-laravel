8f460fb047d98ccbcd63e07c1b8ea821bcebb493
Bruno Galache Theodoro <bruno.hgt2@gmail.com>
2026-09-21 15:05:51 -0300

    auth-validation

--- resources/js/pages/Auth.vue
+++ resources/js/pages/Auth.vue
@@ -7,10 +7,14 @@
                         <div class="w-1/2 shrink-0 flex justify-center">
                             <div class="w-full max-w-[600px] flex justify-center items-start">
                                 <div class="bg-primaria rounded-[20px] p-5 py-12 lg:p-6 lg:py-12 w-[90%] lg:w-[80%]">
-                                    <div class="flex flex-col justify-center items-center">
+                                    <form @submit.prevent="makeLogin" novalidate class="flex flex-col justify-center items-center">
                                         <input class="bg-secundaria border border-primaria rounded-[8px] my-[5px] placeholder-primaria pl-[10px] w-[90%] p-[5px] lg:p-[8px] lg:w-[75%] placeholder-opacity-60" type="email" :placeholder="t('auth.email')" v-model="login.email">
                                         <input class="bg-secundaria border border-primaria rounded-[8px] my-[5px] placeholder-primaria pl-[10px] w-[90%] p-[5px] lg:p-[8px] lg:w-[75%] placeholder-opacity-60" type="password" :placeholder="t('auth.password')" v-model="login.password">
-                                        <button v-if="!loadingLogin" class="bg-primaria hover:bg-secundaria border-2 border-secundaria rounded-[20px] my-[10px] w-[50%] p-[5px] lg:p-[8px] text-secundaria hover:text-primaria font-semibold transition-colors duration-1000 cursor-pointer" @click="makeLogin">
+                                        <p v-if="loginError" class="text-red-500 text-sm my-[5px]">
+                                            {{ loginError }}
+                                        </p>
+                                        
+                                        <button v-if="!loadingLogin" type="submit" class="bg-primaria hover:bg-secundaria border-2 border-secundaria rounded-[20px] my-[10px] w-[50%] p-[5px] lg:p-[8px] text-secundaria hover:text-primaria font-semibold transition-colors duration-1000 cursor-pointer">
                                             {{ t('auth.loginButton') }}
                                         </button>
 
@@ -18,27 +22,31 @@
                                             <span class="loader"></span>
                                         </div>
                                         <p @click="cadastrando = true" class="text-secundaria cursor-pointer">{{ t('auth.noAccount') }}</p>
-                                    </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
                         <div class="w-1/2 shrink-0 flex justify-center">
                             <div class="w-full max-w-[600px] flex justify-center items-start">
                                 <div class="bg-primaria rounded-[20px] p-5 py-12 lg:p-6 lg:py-12 w-[90%] lg:w-[80%]">
-                                    <div class="flex flex-col justify-center items-center">
+                                    <form @submit.prevent="makeRegister" novalidate class="flex flex-col justify-center items-center">
                                         <input class="bg-secundaria border border-primaria rounded-[8px] my-[5px] placeholder-primaria pl-[10px] w-[90%] p-[5px] lg:p-[8px] lg:w-[75%] placeholder-opacity-60" type="text" :placeholder="t('auth.name')" v-model="register.name">
                                         <input class="bg-secundaria border border-primaria rounded-[8px] my-[5px] placeholder-primaria pl-[10px] w-[90%] p-[5px] lg:p-[8px] lg:w-[75%] placeholder-opacity-60" type="email" :placeholder="t('auth.email')" v-model="register.email">
                                         <input class="bg-secundaria border border-primaria rounded-[8px] my-[5px] placeholder-primaria pl-[10px] w-[90%] p-[5px] lg:p-[8px] lg:w-[75%] placeholder-opacity-60" type="password" :placeholder="t('auth.password')" v-model="register.password">
                                         <input class="bg-secundaria border border-primaria rounded-[8px] my-[5px] placeholder-primaria pl-[10px] w-[90%] p-[5px] lg:p-[8px] lg:w-[75%] placeholder-opacity-60" type="password" :placeholder="t('auth.confirmPassword')" v-model="register.confirm_password">
-                                        <button v-if="!loadingRegister" class="bg-primaria hover:bg-secundaria border-2 border-secundaria rounded-[20px] my-[10px] w-[50%] p-[5px] lg:p-[8px] text-secundaria hover:text-primaria font-semibold transition-colors duration-1000 cursor-pointer" @click="makeRegister">
+                                        <p v-if="registerError" class="text-red-500 text-sm my-[5px]">
+                                            {{ registerError }}
+                                        </p>
+                                        
+                                        <button v-if="!loadingRegister" type="submit" class="bg-primaria hover:bg-secundaria border-2 border-secundaria rounded-[20px] my-[10px] w-[50%] p-[5px] lg:p-[8px] text-secundaria hover:text-primaria font-semibold transition-colors duration-1000 cursor-pointer">
                                             {{ t('auth.registerButton') }}
                                         </button>
 
-                                        <div v-else class="my-[15px] w-[50%] mt- p-[5px] lg:p-[8px] flex justify-center items-center text-secundaria">
+                                        <div v-else class="my-[15px] w-[50%] p-[5px] lg:p-[8px] flex justify-center items-center text-secundaria">
                                             <span class="loader"></span>
                                         </div>
                                         <p @click="cadastrando = false" class="text-secundaria cursor-pointer">{{ t('auth.alreadyAccount') }}</p>
-                                    </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
@@ -62,6 +70,10 @@
 
     const loadingLogin = ref(false);
     const loadingRegister = ref(false);
+
+    const loginError = ref('');
+    const registerError = ref('');
+
     const leaving = ref(false);
     const entering = ref(true);
 
@@ -78,10 +90,85 @@
         confirm_password:''
     })
 
+    const hasEmptyFields = (data) => {
+        return Object.values(data).some(value => !value.trim());
+    }
+
+    const hasInvalidEmail = (email) => {
+        return !email.includes('@');
+    }
+
     const makeRegister = async() => {
-        const response = await axios.post('/register', register.value);
-        console.log(response.data)
-        router.push('/');
+        registerError.value = '';
+
+        if (hasEmptyFields(register.value)) {
+            registerError.value = t('auth.emptyFields');
+            return;
+        }
+
+        if (hasInvalidEmail(register.value.email)) {
+            registerError.value = t('auth.invalidEmail');
+            return;
+        }
+
+        loadingRegister.value = true;
+
+        try {
+            const response = await axios.post('/register', register.value);
+            console.log(response.data)
+
+            leaving.value = true;
+
+            setTimeout(() => {
+                router.push('/');
+            }, 1000);
+         } catch (error) {
+            loadingRegister.value = false;
+
+            const code = error.response?.data?.code;
+
+            if (code === 'name_required') {
+                registerError.value = t('auth.nameRequired');
+                return;
+            }
+
+            if (code === 'email_required') {
+                registerError.value = t('auth.emailRequired');
+                return;
+            }
+
+            if (code === 'invalid_email') {
+                registerError.value = t('auth.invalidEmail');
+                return;
+            }
+
+            if (code === 'email_exists') {
+                registerError.value = t('auth.emailExists');
+                return;
+            }
+
+            if (code === 'password_required') {
+                registerError.value = t('auth.passwordRequired');
+                return;
+            }
+
+            if (code === 'password_min') {
+                registerError.value = t('auth.passwordMin');
+                return;
+            }
+
+            if (code === 'password_confirmation_required') {
+                registerError.value = t('auth.passwordConfirmationRequired');
+                return;
+            }
+
+            if (code === 'password_confirmation') {
+                registerError.value = t('auth.passwordConfirmation');
+                return;
+            }
+
+            registerError.value = t('auth.validationError');
+        }
     }
 
 
@@ -91,6 +178,17 @@
     })
 
     const makeLogin = async() => {
+        loginError.value = '';
+
+        if (hasEmptyFields(login.value)) {
+            loginError.value = t('auth.emptyFields');
+            return;
+        }
+
+        if (hasInvalidEmail(login.value.email)) {
+            loginError.value = t('auth.invalidEmail');
+            return;
+        }
 
         loadingLogin.value = true;
 
@@ -99,15 +197,27 @@
             console.log(response.data)
 
             leaving.value = true;
+
             setTimeout(() => {
                 router.push('/');
             }, 1000);
-        } catch (error){
+        } catch (error) {
             loadingLogin.value = false;
 
-            console.log(error);
-        }
+            const code = error.response?.data?.code;
 
+            if (code === 'email_not_found') {
+                loginError.value = t('auth.emailNotFound');
+                return;
+            }
+
+            if (code === 'invalid_password') {
+                loginError.value = t('auth.invalidPassword');
+                return;
+            }
+
+            loginError.value = t('auth.validationError');
+        }
     }
 
 
